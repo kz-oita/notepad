07b5d1f7f232bda50937923a555635404f6a9e3d
kz-oita <taoi.kizuka@gmail.com>
2020-06-02 09:18:23 +0900

    complete commit

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="editor">
-    <Header>New Memo</Header>
+    <Header>Edit memo</Header>
     <textarea name="memo" v-model="memoBody"></textarea>
     <button @click="save">保存</button>
-
   </div>
-
 </template>
-
 
 
 <script>
 import Header from "@/components/Header.vue"
 
 export default {
-  name: "new",
+  name: 'edit',
   components: {
     Header
   },
   data: function() {
     return {
-      memoBody: ""
+      memoBody: ''
     }
+  },
+  mounted: function() {
+    let id = this.$route.params["id"];
+    let memo = this.$store.state.memos.find(memo => memo.id == id);
+    this.memoBody = memo.body;
   },
   methods: {
     save: function() {
-      this.$store.commit("save", {
+      this.$store.commit("update", {
+        id: this.$route.params["id"],
         body: this.memoBody
-      })
-      this.$router.push("/")
+      });
+      this.$router.push('/');
     }
   }
+
 }
 </script>
